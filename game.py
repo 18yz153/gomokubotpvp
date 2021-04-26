a809958821979cbe176197790bed5673409485e2
18yz153 <>
2021-04-26 12:50:08 +0000

    player can reply now

---
+++ game.py
@@ -0,0 +1,37 @@
+class game():
+  def __init__(self):
+    self.board = [None]*15
+    for i in range(len(self.board)):
+      self.board[i]=["+ "]*15
+    
+  def addchess(self,chess,pos):
+    x=int(pos[0])-1
+    y=int(pos[1])-1
+    if self.board[x][y]=="+ ":
+      self.board[x][y]=chess
+
+  def iscontiune(self):
+    for i in range(len(self.board)):
+        for j in range(len(self.board[i])):
+            if self.board[i][j]!='+ ':
+                if j<=11:
+                    if self.board[i][j]==self.board[i][j+1]==self.board[i][j+2]==self.board[i][j+3]==self.board[i][j+4]:
+                        return False
+                if i<=11:
+                    if self.board[i][j]==self.board[i+1][j]==self.board[i+2][j]==self.board[i+3][j]==self.board[i+4][j]:
+                        return False
+                if i<=11 and j<=11:
+                    if self.board[i][j]==self.board[i][j+1]==self.board[i][j+2]==self.board[i][j+3]==self.board[i][j+4]:
+                        return False
+                if i>=4 and j<=11:
+                    if self.board[i][j]==self.board[i][j+1]==self.board[i][j+2]==self.board[i][j+3]==self.board[i][j+4]:
+                        return False
+    return True
+
+
+  
+  def __str__(self):
+    boardimg = '\n'.join(''.join(map(str,i)) for i in self.board)
+    return boardimg
+
+
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
 import os
+import game
+import asyncio
 
 from dotenv import load_dotenv
 
@@ -8,15 +10,49 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+
 @client.event
 async def on_message(m):
-  if m.author == client.user:
-    return
-  if m.content.startswith('inters'):
-    await m.channel.send('sb')
+    if m.author == client.user:
+        return
+    if m.content.startswith('-p'):
+        p1 = m.author
+        player2l = m.mentions
+        if len(player2l) == 1:
+            p2 = player2l[0]
+            await m.channel.send(p1.name + ' play with ' + p2.name)
+
+            def isp1(mess):
+                return mess.author == p1
+
+            def isp2(mess):
+                return mess.author == p2
+
+            g = game.game()
+            await m.channel.send(g)
+            turn = 1
+            while (g.iscontiune()):
+                print('round')
+                if turn == 1:
+                    try:
+                        pos = await client.wait_for('m', check=isp1, timeout=5)
+                    except asyncio.TimeoutError:
+                        return await m.channel.send('timeout')
+                    g.addchess('●', pos)
+                    turn = 0
+                else:
+                    try:
+                        pos = await client.wait_for('m', check=isp2, timeout=5)
+                    except asyncio.TimeoutError:
+                        return await m.channel.send('timeout')
+                    g.addchess('○', pos)
+                    turn = 1
+                await m.channel.send(g)
+
 
 client.run(TOKEN)
